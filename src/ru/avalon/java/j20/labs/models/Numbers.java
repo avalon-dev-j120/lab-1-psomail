382e829670302191815aeaba26eebb4086590f15
Sergei Perminov <psomail@yandex.ru>
2019-12-06 21:57:37 +0300

    fix min and max

--- src/ru/avalon/java/j20/labs/models/Numbers.java
+++ src/ru/avalon/java/j20/labs/models/Numbers.java
@@ -38,8 +38,8 @@ public final class Numbers {
      * @param b второе значение
      * @return большее из двух значений
      */
-    public static int max(int a, int b) {
-        return a > b ? a : b;
+    public static <T extends Number> T max(T a, T b) {
+        return  a.doubleValue() > b.doubleValue() ? a : b;
     }
 
     /**
@@ -48,10 +48,10 @@ public final class Numbers {
      * @param values массив значений
      * @return максимальное значение массива
      */
-    public static <T extends Number> int max(T[] values) {
-        int result = values[0].intValue();
+    public static <T extends Number> T max(T[] values) {
+        T result = values[0];
         for (int i = 1; i < values.length; i++) {
-            result = max(result, values[i].intValue());
+            result = max(result, values[i]);
         }
         return result;
     }
@@ -63,8 +63,9 @@ public final class Numbers {
      * @param b второе значение
      * @return меньшее из дух значений
      */
-    public static int min(int a, int b) {
-        return a < b ? a : b;
+    public static <T extends Number> T min(T a, T b) {
+
+        return a.doubleValue() < b.doubleValue() ? a : b;
     }
 
     /**
@@ -73,10 +74,10 @@ public final class Numbers {
      * @param values массив значений
      * @return минимальное значение массива
      */
-    public static <T extends Number> int min(T[] values) {
-        int result = values[0].intValue();
+    public static <T extends Number> T min(T[] values) {
+        T result = values[0];
         for (int i = 1; i < values.length; i++) {
-            result = min(result, values[i].intValue());
+            result = min(result, values[i]);
         }
         return result;
     }
